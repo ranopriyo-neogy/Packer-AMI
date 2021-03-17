e9daa377dae54684df06ab849f6b9a4ef31dfc41
Ranopriyo Neogy <ranopriyoneogy@Tor-Ranopriyo-MBP13.local>
2021-03-17 18:58:25 -0400

    initial commit

---
+++ apache.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo yum install httpd -y
+sudo service httpd start
+sudo chkconfig httpd on 
+
+sudo touch /var/www/html/index.html
+sudo chmod 777 /var/www/html/index.html
+
+echo "<h1> This app is deployed by packer </h1>" > /var/www/html/index.html
