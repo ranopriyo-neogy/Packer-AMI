d3ee95ee8b0fc4328f3e9b6eb5d377c6eb1179c8
Ranopriyo Neogy <ranopriyoneogy@Tor-Ranopriyo-MBP13.local>
2021-03-23 18:10:41 -0400

    code updated

--- nginx.sh
+++ nginx.sh
@@ -5,5 +5,3 @@ sudo amazon-linux-extras install nginx1 -y
 sudo systemctl start nginx
 sudo systemctl enable nginx 
 sudo systemctl status nginx
-
-echo "<h1> This app is deployed on Nginx by packer </h1>" > /var/www/html/index.html
